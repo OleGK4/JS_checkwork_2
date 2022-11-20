815e6163a3bdbda26881db185d21b46b63faa3af
Oleg <oleg.krut.64@gmail.com>
2022-11-20 18:56:18 +0700

    Конец

--- js.js
+++ js.js
@@ -1,136 +1,181 @@
 class PhoneUser {
-    constructor(surname, name, patronymic, address, phone_number) {
+    constructor(kazakh, surname, name, patronymic, phone_number, country_index, region, city, block, street, house, apartment, floor) {
         this.users = [];
-        this.toAddusers = [
+        this.defaultUsers = [
             {
+                'kazakh': true,
                 'surname': 'Василевсов',
                 'name': 'Василий',
                 'patronymic': 'Васильев',
                 'phone_number': 4562345123,
-                'address': {
-                    "country_index": '2343',
-                    "region": 'Капусткая обл.',
-                    "block": 'Водный',
-                    "city": 'Картошков',
-                    "street": 'Капустянская',
-                    "house": '45',
-                    "apartment": '215',
-                    "floor": '10',
-                }
+                'country_index': '2343',
+                'region': 'Капусткая обл.',
+                'city': 'Картошков',
+                'block': 'Водный',
+                'street': 'Капустянская',
+                'house': '45',
+                'apartment': '215',
+                'floor': '10',
             },
             {
+                'kazakh': false,
                 'surname': 'Максимов',
                 'name': 'Максим',
                 'patronymic': 'Максимович',
                 'phone_number': 4562345321,
-                'address': {
-                    "country_index": '2343',
-                    "region": 'Кабачковая обл.',
-                    "block": 'Известняковый',
-                    "city": 'Морковец',
-                    "street": 'Ручковенская',
-                    "house": '12',
-                    "apartment": '115',
-                    "floor": '4',
-                }
+                'country_index': '2343',
+                'region': 'Кабачковая обл.',
+                'city': 'Морковец',
+                'block': 'Известняковый',
+                'street': 'Ручковенская',
+                'house': '12',
+                'apartment': '115',
+                'floor': '4',
             },
             {
+                'kazakh': false,
                 'surname': 'Максимов',
                 'name': 'Максим',
                 'patronymic': 'Максимович',
                 'phone_number': 4562345321,
-                'address': {
-                    "country_index": '2343',
-                    "region": 'Кабачковая обл.',
-                    "block": 'Известняковый',
-                    "city": 'Морковец',
-                    "street": 'Ручковенская',
-                    "house": '12',
-                    "apartment": '115',
-                    "floor": '4',
-                }
+                'country_index': '2343',
+                'region': 'Кабачковая обл.',
+                'city': 'Морковец',
+                'block': 'Известняковый',
+                'street': 'Ручковенская',
+                'house': '12',
+                'apartment': '115',
+                'floor': '4',
             },
         ];
+        this.kazakh = kazakh;
         this.surname = surname;
         this.name = name;
         this.patronymic = patronymic;
-        this.address = address;
         this.phone_number = phone_number;
-    }
+        this.country_index = country_index;
+        this.region = region;
+        this.city = city;
+        this.block = block;
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+        this.floor = floor;
+    };
 
     menu() {
         let a = confirm("Нужно ли ввести стандартных пользователей?");
         if (a === true) {
             switch (+prompt('Введите количество пользователей для заполнения (1 или 3)', '2')) {
                 case 1:
-                    this.users.push(...this.toAddusers[0]);
+                    this.users.push(...this.defaultUsers.slice(0, 1));
                     break
                 case 2:
-                    this.users.push(...this.toAddusers.slice(1, 0));
+                    this.users.push(...this.defaultUsers.slice(0, 2));
                     break;
                 case 3:
-                    this.users.push(...this.toAddusers);
+                    this.users.push(...this.defaultUsers);
                     break;
             }
         }
-        while(true) {
+        while (true) {
             switch (+prompt(`
             1. Вывести пользователей в консоль
-            2. Ввод нового пользователя
-            3. Ввод ограниченного количества пользователей
-            4. Вывести только казахов
-            5. Выход
+            2. Ввод новых пользователей, ограниченных по количеству
+            3. Вывести только казахов
+            4. Выход
             `)) {
                 case 1:
-                    newUser.userList();
+                    newUser.displayUsers();
                     break;
                 case 2:
-                    newUser.newUser();
+                    newUser.createUsers();
                     break;
                 case 3:
-                    newUser.newCappedUser();
-                    break;
-                case 4:
-                    newUser.userFiltered();
+                    newUser.filterUsers();
                     break;
                 default:
+                    console.log('Выход');
                     return false;
             }
         }
-    }
-    userList(){
+    };
+
+    displayUsers() {
         let count = 1;
-        for(let i of this.users){
+        for (let i of this.users) {
             console.log(`
-                                                     № Пользователя: ${count++}
+                             № Пользователя: ${count++}
+            Казах ли?: ${i.kazakh}
             Фамилия: ${i.surname}
             Имя: ${i.name}  
             Отчество: ${i.patronymic}
-            Номер телефона: +7${i.phone_number}
-            Адрес:\n 
-                    Индекс страны: ${i.address.country_index}
-                    Область: ${i.address.region}
-                    Район: ${i.address.block}
-                    Город: ${i.address.city}
-                    Улица: ${i.address.street}
-                    Дом: ${i.address.house}
-                    Квартира: ${i.address.apartment}
-                    Этаж: ${i.address.floor}                                
-            `)
+            Номер телефона: +7${i.phone_number}\n
+            Адрес:
+                Индекс страны: ${i.country_index}
+                Область: ${i.region}
+                Город: ${i.city}
+                Район: ${i.block}                 
+                Улица: ${i.street}
+                Дом: ${i.house}
+                Квартира: ${i.apartment}
+                Этаж: ${i.floor}                                
+            `);
         }
-    }
-    newUser(){
-        let number = +prompt('Количество пользователей для заполнения','1');
-        for(let i = 1; i <= number; i++){
+    };
+
+    createUsers() {
+        let number = +prompt('Количество пользователей для заполнения', '1');
+        for (let i = 1; i <= number; i++) {
             this.users.push(new PhoneUser(
-                prompt('Фамилия:','Собаков'),
-                prompt('Имя:','Собака'),
-                prompt('Отчество:','Собакович'),
-                prompt('Адрес:','shgfh'),
-                +prompt('Номер телефона:','9839839838'),
-            ))
+                confirm('КАЗАХ?'),
+                prompt('Фамилия:', 'Собаков'),
+                prompt('Имя:', 'Собака'),
+                prompt('Отчество:', 'Собакович'),
+                +prompt('Номер телефона:', '9839839838'),
+                +prompt('Индекс страны:', '6767'),
+                prompt('Область:', 'Комаровская'),
+                prompt('Город:', 'Косячев'),
+                prompt('Район:', 'Соляной'),
+                prompt('Улица:', 'Нариковая'),
+                +prompt('Дом:', '228'),
+                +prompt('Квартира:', '322'),
+                +prompt('Этаж:', '15'),
+            ));
         }
-    }
+    };
+
+    filterUsers() {
+        let filteredUsers = [];
+        for (let i of this.users) {
+            if (i.kazakh === true) {
+                filteredUsers.push(i)
+            }
+        }
+        let c = confirm('Вывести казахов?')
+        if (c === true) {
+            let count = 1;
+            for (let i of filteredUsers) {
+                console.log(`
+                             № Пользователя: ${count++}
+            Казах ли?: ${i.kazakh}
+            Фамилия: ${i.surname}
+            Имя: ${i.name}  
+            Отчество: ${i.patronymic}
+            Номер телефона: +7${i.phone_number}\n
+            Адрес:
+                Индекс страны: ${i.country_index}
+                Область: ${i.region}
+                Город: ${i.city}
+                Район: ${i.block}                 
+                Улица: ${i.street}
+                Дом: ${i.house}
+                Квартира: ${i.apartment}
+                Этаж: ${i.floor}                   
+            `)
+            }
+        }
+    };
 }
 
 let newUser = new PhoneUser();
